2e44d335de556333256e5b6858261c60a8849a70
yashuk803@gmail.com <marmeladik12>
2019-03-13 20:46:37 +0200

    add Book entity and relation with User

---
+++ src/entity/book.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from './user';
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column({ type: 'date' })
+    date: Date;
+
+    @ManyToOne(type => User, user => user.books)
+    user: User;
+}
