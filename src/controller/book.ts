f3e8219a3c9348dbfd190a0b2b5459513e7bd92e
yashuk803@gmail.com <marmeladik12>
2019-03-13 23:12:09 +0200

    create method add book in db, library dateformat

--- src/controller/book.ts
+++ src/controller/book.ts
@@ -1,4 +1,5 @@
 import { BaseContext } from 'koa';
+import { dateformat } from 'dateformat';
 import { getManager, Repository, Not, Equal } from 'typeorm';
 import { validate, ValidationError } from 'class-validator';
 import { User } from '../entity/user';
@@ -23,6 +24,35 @@ export default class BookController {
         // return OK status code and loaded user object
         ctx.status = 200;
         ctx.body = userBooks;
+
+    }
+
+    public static async createUserBook (ctx: BaseContext) {
+
+        const userRepository: Repository<User> = getManager().getRepository(User);
+
+        const bookRepository: Repository<Book> = getManager().getRepository(Book);
+        // build up entity book to be saved
+        const bookToBeSaved: Book = new Book();
+        bookToBeSaved.name = ctx.request.body.name;
+        bookToBeSaved.description = ctx.request.body.description;
+        bookToBeSaved.date = ctx.request.body.date || dateformat(new Date(), 'yyyy-mm-dd');
+        bookToBeSaved.user = ctx.params.id;
+
+        if(! await userRepository.findOne({ id: ctx.params.id}) ) {
+            ctx.status = 400;
+            ctx.body = 'This user doesn\'t in db ';
+        } else if ( await bookRepository.findOne({ name: bookToBeSaved.name}) ) {
+
+            ctx.status = 400;
+            ctx.body = 'This book already exit ';
+
+        } else {
+
+            const book = await bookRepository.save(bookToBeSaved);
+            ctx.status = 201;
+            ctx.body = book;
+        }
 
     }
 }
